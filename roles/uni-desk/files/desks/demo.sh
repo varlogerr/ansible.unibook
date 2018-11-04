b73321881c487906c879dbd769a423e1645cc636
Varlog <varlog.err@gmail.com>
2018-11-04 23:30:44 +0200

    refactor desks naming

--- roles/uni-desk/files/desks/demo.sh
+++ roles/uni-desk/files/desks/demo.sh
@@ -3,9 +3,6 @@
 # Description: demo desk
 #
 
-desk_name=demo
-export PS1="${PS1}\[\033[01;34m\]${desk_name}\[\033[00m\] > "
-
 cd ~
 
 print_demo() {
@@ -14,3 +11,5 @@ print_demo() {
 }
 
 print_demo
+
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/base"
--- roles/uni-desk/files/desks/golang.sh
+++ roles/uni-desk/files/desks/golang.sh
@@ -3,10 +3,7 @@
 # Description: golang desk
 #
 
-desk_name=golang
 project_path=`pwd`
-
-export PS1="${PS1}\[\033[01;34m\]${desk_name}\[\033[00m\] > "
 
 export GOPATH=$project_path
 export GOBIN="$GOPATH/bin"
@@ -14,3 +11,5 @@ export GOBIN="$GOPATH/bin"
 gen-goproject() {
     mkdir "${GOPATH}"/{bin,src,pkg}
 }
+
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/base"
