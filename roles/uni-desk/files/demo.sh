2726093583f6ab6a2dba43836ce7f29046003ff6
Varlog <varlog.err@gmail.com>
2018-11-04 20:15:17 +0200

    create desk role

---
+++ roles/uni-desk/files/demo.sh
@@ -0,0 +1,17 @@
+# demo.sh
+#
+# Description: demo desk
+#
+
+desk_name=demo
+
+export PS1="${PS1}\[\033[01;34m\]${desk_name}\[\033[00m\] > "
+
+cd ~
+
+print_demo() {
+  export DEMO=demo
+  echo $DEMO
+}
+
+print_demo
